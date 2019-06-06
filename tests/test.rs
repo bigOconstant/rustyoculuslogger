611f634b76ca09fc6fd41e33cfb323add6e93445
Caleb McCarthy <caleb.andrew.mccarthy@ibm.com>
2019-06-06 09:24:20 -0500

    Adding pre commit hook stuff

--- tests/test.rs
+++ tests/test.rs
@@ -1,18 +1,36 @@
 use rustoculuslogger;
 
-#[test]
-fn test_print() {
-    let logobject = rustoculuslogger::LogObject::new()
-                            .msg("Error message".to_string())
-                            .op("Location of error message".to_string())
-                            .lvl("error".to_string())
-                            .optional_type("Mccarthy".to_string(),"was stillhere".to_string())
-                            .optionaldata("Optional data".to_string());
+// #[test]
+// fn test_print() {
+//     let logobject = rustoculuslogger::LogObject::new()
+//                             .msg("Error message".to_string())
+//                             .op("Location of error message".to_string())
+//                             .lvl("error".to_string())
+//                             .optional_type("Mccarthy".to_string(),"was stillhere".to_string())
+//                             .optionaldata("Optional data".to_string());
 
                             
     
     
-    logobject.print();
+//     logobject.print();
+
+//     //logobject.map_test();
+//     assert_eq!(1, 1);// Just testing a print out
+// }
+
+#[test]
+fn test_print_info() {
+    let logobject = rustoculuslogger::LogObject::new()
+                           .msg("Committing offsets".to_string())
+                            .op("ConsumerContext".to_string())
+                            .lvl("INFO".to_string())
+                            //.optionaldata(result.to_string())
+                            .print();
+
+                            
+    
+    
+    //logobject.print();
 
     //logobject.map_test();
     assert_eq!(1, 1);// Just testing a print out
